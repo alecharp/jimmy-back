0cb3a392a68ef829b9ba54eff5849e56a155d812
Adrien Lecharpentier <adrien.lecharpentier@gmail.com>
2018-10-31 21:19:54 +0100

    Use WebFlux

--- src/main/java/fr/alecharp/jimmy/back/http/PingAPI.java
+++ src/main/java/fr/alecharp/jimmy/back/http/PingAPI.java
@@ -16,21 +16,17 @@
 
 package fr.alecharp.jimmy.back.http;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.keycloak.adapters.springsecurity.token.KeycloakAuthenticationToken;
 import org.springframework.http.MediaType;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
 
 @RestController
 public class PingAPI {
-    private static final Logger LOG = LoggerFactory.getLogger(PingAPI.class);
-
     @GetMapping(value = "/api/ping", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public String ping(@AuthenticationPrincipal User user) {
-        LOG.debug("{}", user);
-        return "pong";
+    public Mono<String> ping(@AuthenticationPrincipal KeycloakAuthenticationToken user) {
+        return Mono.just("Hello " + user.getName());
     }
 }
